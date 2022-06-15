8dd7d17471fba7221cce949294fdb1c6f32f7028
CaliaK <ckugler1@binghamton.edu>
2022-06-14 20:26:10 -0400

    changed about me to mine

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import picture from "../assets/IMG_5676.jpeg"; 
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
+         <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {picture}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Calia Kugler</div>
+        <div className="brief_description"> Hi! I am an incoming softmore double majoring in math and computer science, with a spanish minor.
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+        <div className="brief_description_pt_2"> As a fun fact, during my free time I love ballroom dancing! I am even on my schools ballroom dacning team!
         </div>
       </div>
-    );
+    </div>
+  </div>
+      </div>
+    )
   }
 }
